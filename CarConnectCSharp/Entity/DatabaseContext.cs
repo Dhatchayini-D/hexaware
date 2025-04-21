54476f1174d0714d4154aefa1b214f47c8de22ed
Dhatchayini-D <158227413+Dhatchayini-D@users.noreply.github.com>
2025-04-21 10:58:31 +0530

    Add files via upload

---
+++ CarConnectCSharp/Entity/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarConnectCSharp.Entity
+{
+    class DatabaseContext
+    {
+        string connectionString = "Server=DHATCHAYINI;Database=CarConnect;Integrated Security=True;TrustServerCertificate=True";
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
---
+++ CarConnectCSharp/Util/DButil.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarConnectCSharp.Util
+{
+    class DButil
+    {
+        public static SqlConnection GetDBConn()
+        {
+            string connectionString = "Server=DHATCHAYINI;Database=CarConnect;Integrated Security=True;TrustServerCertificate=True";
+
+            SqlConnection connection = null!;
+            try
+            {
+                connection = new SqlConnection(connectionString);
+                connection.Open();
+                Console.WriteLine(" Database connection established successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error connecting to the database: " + ex.Message);
+            }
+            return connection;
+        }
+    }
+}
+
